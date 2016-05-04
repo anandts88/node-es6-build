ff16f48873ec908f9ccae1a006e6886ea4475a4c
Anand Sakthivel <anandts88@gmail.com>
2016-05-04 15:57:01 -0400

    authorization

--- app/controllers/authentication.js
+++ app/controllers/authentication.js
@@ -4,6 +4,8 @@ import environment from '../config/environment';
 import ApiError from '../errors/api-error';
 import User from '../models/user';
 
+const { FORBIDDEN, UNAUTHORIZED } = httpStatus;
+
 function authenticate(req, res, next) {
   const tokenHeader = req.headers[environment.authTokenHeader];
   const sessionToken = req.session.token;
@@ -12,33 +14,33 @@ function authenticate(req, res, next) {
 
   if (!tokenHeader) {
     return next(
-      new ApiError('Authentication fails', httpStatus.UNAUTHORIZED));
+      new ApiError('Authentication fails', UNAUTHORIZED));
   }
 
   try {
 
     if (tokenHeader !== sessionToken) {
       return next(
-        new ApiError('Authentication fails', httpStatus.UNAUTHORIZED));
+        new ApiError('Authentication fails', UNAUTHORIZED));
     }
 
     token = jwtSimple.decode(tokenHeader, environment.secretKey);
 
     if (token.expires <= Date.now()) {
 			return next(
-        new ApiError('Authentication fails', httpStatus.UNAUTHORIZED));
+        new ApiError('Authentication fails', UNAUTHORIZED));
 		}
 
     userName = token.userName;
 
     if (!userName) {
       return next(
-        new ApiError('Authentication fails', httpStatus.UNAUTHORIZED));
+        new ApiError('Authentication fails', UNAUTHORIZED));
     }
 
     if (req.session.user.userName !== userName) {
       return next(
-        new ApiError('Authentication fails', httpStatus.UNAUTHORIZED));
+        new ApiError('Authentication fails', UNAUTHORIZED));
     }
 
     User
@@ -47,7 +49,7 @@ function authenticate(req, res, next) {
       .then((user) => {
         if (!user) {
           return next(
-            new ApiError('Authentication fails', httpStatus.UNAUTHORIZED));
+            new ApiError('Authentication fails', UNAUTHORIZED));
         }
 
         next();
@@ -57,8 +59,19 @@ function authenticate(req, res, next) {
       });
 
   } catch (err) {
-    return next(new ApiError('Authentication fails', httpStatus.UNAUTHORIZED));
+    return next(new ApiError('Authentication fails', UNAUTHORIZED));
   }
 }
 
-export default { authenticate };
+function authorize(roles) {
+  return (req, res, next) => {
+    const { user } = req.session;
+
+    if (roles && roles.indexOf(user.role) === -1) {
+      return next(new ApiError('Authorization fails', FORBIDDEN));
+    }
+    next();
+  };
+}
+
+export default { authenticate, authorize };
